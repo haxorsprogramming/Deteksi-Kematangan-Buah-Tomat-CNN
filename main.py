0577b97f475026dfab2256b034841a7d64a0af62
Haxorsprogramming <haxors@uinsu.ac.id>
2022-12-26 21:19:29 +0700

    update dari notnot

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 from flask import Flask, redirect, url_for, render_template, request, jsonify, flash
+from werkzeug.utils import secure_filename
+from operasiFile import hitungTotalFile
+
+import pandas as pd
+import numpy as np
+
+import os
+import uuid
+import base64
---
+++ operasiFile.py
@@ -0,0 +1,2 @@
+def hitungTotalFile():
+    return 0
